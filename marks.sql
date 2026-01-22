fc6cd01f79cc1d6c39c96d4368df8eb05b1b07d4
Ragukumaran Piratheep <142398970+Ragukumaran-Piratheep@users.noreply.github.com>
2026-01-22 11:40:35 +0530

    Update marks.sql

--- marks.sql
+++ marks.sql
@@ -104,3 +104,70 @@ MariaDB [student_marks]> describe student;
 | dept_id      | int(11)     | NO   | MUL | NULL    |       |
 +--------------+-------------+------+-----+---------+-------+
 5 rows in set (0.058 sec)
+
+MariaDB [student_marks]> insert into department(dept_id, dept_name, building)
+    -> values
+    -> (1,'Computer Science', 'Block A'),
+    -> (2, 'Mathematics', 'block B'),
+    -> (3, 'Physics', 'Block C');
+Query OK, 3 rows affected (0.187 sec)
+Records: 3  Duplicates: 0  Warnings: 0
+
+MariaDB [student_marks]> insert into student(
+    -> stduent_id, student_name, age, marks, dept_id)
+    -> values
+    -> (101, 'Alice', 20, 85, 1),
+    -> (102, 'Bob', 22, 78, 1),
+    -> (103, 'Charlie', 21, 92, 2),
+    -> (104, 'David', 20, 65, 2),
+    -> (105, 'Eva', 20, 88, 3);
+Query OK, 5 rows affected (0.137 sec)
+Records: 5  Duplicates: 0  Warnings: 0
+
+MariaDB [student_marks]> select * from deprtment;
+
+    
+MariaDB [student_marks]> select * from department;
++---------+------------------+----------+
+| dept_id | dept_name        | building |
++---------+------------------+----------+
+|       1 | Computer Science | Block A  |
+|       2 | Mathematics      | block B  |
+|       3 | Physics          | Block C  |
++---------+------------------+----------+
+3 rows in set (0.001 sec)
+
+MariaDB [student_marks]> select * from student;
++------------+--------------+------+-------+---------+
+| stduent_id | student_name | age  | marks | dept_id |
++------------+--------------+------+-------+---------+
+|        101 | Alice        |   20 |    85 |       1 |
+|        102 | Bob          |   22 |    78 |       1 |
+|        103 | Charlie      |   21 |    92 |       2 |
+|        104 | David        |   20 |    65 |       2 |
+|        105 | Eva          |   20 |    88 |       3 |
++------------+--------------+------+-------+---------+
+5 rows in set (0.001 sec)
+MariaDB [student_marks]> select * from student where student_name like 'A%';
++------------+--------------+------+-------+---------+
+| stduent_id | student_name | age  | marks | dept_id |
++------------+--------------+------+-------+---------+
+|        101 | Alice        |   20 |    85 |       1 |
++------------+--------------+------+-------+---------+
+1 row in set (0.000 sec)
+
+MariaDB [student_marks]> select age, marks from student where age > 20 and marks > 80;
++------+-------+
+| age  | marks |
++------+-------+
+|   21 |    92 |
++------+-------+
+1 row in set (0.001 sec)
+
+MariaDB [student_marks]> select * from student where age > 20 and marks > 80;
++------------+--------------+------+-------+---------+
+| stduent_id | student_name | age  | marks | dept_id |
++------------+--------------+------+-------+---------+
+|        103 | Charlie      |   21 |    92 |       2 |
++------------+--------------+------+-------+---------+
+1 row in set (0.001 sec)
